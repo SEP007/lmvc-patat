9c17fa6d113f18b45e3bd8873e55b7556e8910c3
anna-averianova <ann.averyanova@gmail.com>
2013-11-26 09:39:37 +0100

    Displays travel time on imbiss index page

--- app/javascripts/views/jquery.time-to-get.js
+++ app/javascripts/views/jquery.time-to-get.js
@@ -2,9 +2,6 @@
  * Uses Goggle Maps API to calculate journey duration
  * https://google-developers.appspot.com/maps/documentation/javascript/distancematrix?hl=en
  */
-
-var timeLeftElem = document.getElementById("time-left");
-
 function calc_time(long1, long2, lat1, lat2)
 {
     var destination = new google.maps.LatLng(lat1, long1);
@@ -24,6 +21,8 @@ function calc_time(long1, long2, lat1, lat2)
 }
 
 function callback(response, status) {
+    var timeLeftElem = document.getElementById("time-left");
+
     if (status != google.maps.DistanceMatrixStatus.OK) {
         timeLeftElem.innerHTML = 'Error was: ' + status;
     } else {
@@ -37,6 +36,20 @@ function callback(response, status) {
                 var duration = element.duration.text;
             }
         }
-        timeLeftElem.innerHTML = duration.toString();
+        timeLeftElem.innerHTML =duration.toString();
     }
 }
+
+$(function() {
+    // request geo-location
+    navigator.geolocation.watchPosition(function(location) {
+        var $timeLeft = $('#time-left');
+        calc_time(
+            $timeLeft.data('longitude'),
+            location.coords.longitude,
+            $timeLeft.data('latitude'),
+            location.coords.latitude
+        );
+    });
+});
+
