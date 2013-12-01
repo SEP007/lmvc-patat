92281e851a47143482cb98b01e9d342c71e50b13
Iva Popova <iva.svetoslavova@gmail.com>
2013-12-01 22:59:21 +0100

    Adds sql script for the new rating table Fixes hover cursor on Rate me link

---
+++ docs/restaurant_rating.sql
@@ -0,0 +1,24 @@
+# ------------------------------------------------------------
+# Create table Cust_Location_Rating
+# ------------------------------------------------------------
+CREATE TABLE Cust_Location_Rating (
+  location_id int(11) NOT NULL,
+  cust_id int(11) NOT NULL,
+  rating int(11) NULL,
+  PRIMARY KEY (location_id, cust_id),
+  FOREIGN KEY (cust_id) REFERENCES Customers (id)
+  ON UPDATE CASCADE
+  ON DELETE RESTRICT,
+  FOREIGN KEY (location_id) REFERENCES Locations (id)
+  ON UPDATE CASCADE
+  ON DELETE RESTRICT
+);
+
+# Update table Locations
+# ------------------------------------------------------------
+
+ALTER TABLE `Locations`
+ADD COLUMN avg_rating DECIMAL(3,2) NULL;
+
+ALTER TABLE `Locations`
+ADD COLUMN num_votes int(11) NULL;
